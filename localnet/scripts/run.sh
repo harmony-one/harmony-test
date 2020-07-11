099c02a8d36e4abac3ceff90ddcf994e637c2f00
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-07-10 21:14:39 -0700

    [localnet] Speed up test iteration with dynamic builds

--- localnet/scripts/run.sh
+++ localnet/scripts/run.sh
@@ -30,18 +30,25 @@ function build_and_start_localnet() {
   rm -f "$harmony_dir/*.rlp"
   pushd "$(pwd)"
   cd "$harmony_dir"
-  make
+  if [ "$BUILD" == "true" ]; then
+    # Dynamic for faster build iterations
+    bash ./scripts/go_executable_build.sh -S
+  fi
   bash ./test/deploy.sh -B -D 60000 "$localnet_config"  2>&1 | tee "$localnet_log"
   popd
 }
 
 function wait_for_localnet_boot() {
+  timeout=70
+  if [ -n "$1" ]; then
+    timeout=$1
+  fi
   i=0
   until curl --silent --location --request POST "localhost:9500" \
     --header "Content-Type: application/json" \
     --data '{"jsonrpc":"2.0","method":"net_version","params":[],"id":1}' >/dev/null; do
     echo "Trying to connect to localnet..."
-    if ((i > $1 )); then
+    if ((i > timeout )); then
       echo "TIMEOUT REACHED"
       exit 1
     fi
@@ -56,7 +63,7 @@ function wait_for_localnet_boot() {
       --data '{"jsonrpc":"2.0","method":"hmy_blockNumber","params":[],"id":1}' | jq '.result')
     if [ "$result" = "\"0x0\"" ]; then
       echo "Waiting for localnet to boot..."
-      if ((i > $1 )); then
+      if ((i > timeout )); then
         echo "TIMEOUT REACHED"
         exit 1
       fi
@@ -71,6 +78,20 @@ function wait_for_localnet_boot() {
 }
 
 trap kill_localnet SIGINT SIGTERM EXIT
+
+BUILD=true
+while getopts "B" option; do
+  case ${option} in
+  B) BUILD=false;;
+  *) echo "
+Integration tester for localnet
+
+Option:      Help:
+-B           Do NOT build binray before testing
+"
+  esac
+done
+
 
 setup
 build_and_start_localnet || exit 1 &
