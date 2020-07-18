400378e26aed29001b06339823e2c4c79c53c92d
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-07-17 19:49:02 -0700

    [localnet] Fix release_test.sh

--- localnet/scripts/release_test.sh
+++ localnet/scripts/release_test.sh
@@ -4,6 +4,6 @@ set -e
 for i in {1..10}
 do
 	echo "=== START TEST NUMBER ${i} ==="
-	docker run -v "$(go env GOPATH)/src/github.com/harmony-one/harmony:/go/src/github.com/harmony-one/harmony" harmonyone/node-test
+	docker run -v "$(go env GOPATH)/src/github.com/harmony-one/harmony:/go/src/github.com/harmony-one/harmony" harmonyone/localnet-test
 	echo "=== END TEST NUMBER ${i} ==="
 done
