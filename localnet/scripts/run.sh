a9ffd948296d3990435a6a5c42e25b228ad56971
Daniel Van Der Maden <dvandermaden0@berkeley.edu>
2020-10-31 04:00:24 -0700

    [localnet] Wait for staking epoch b4 starting rosetta tests

--- localnet/scripts/run.sh
+++ localnet/scripts/run.sh
@@ -89,7 +89,7 @@ function rosetta_tests() {
   echo -e "\n=== \e[38;5;0;48;5;255mSTARTING ROSETTA API TESTS\e[0m ===\n"
   build_and_start_localnet || exit 1 &
   sleep 30
-  wait_for_localnet_boot 100 # Timeout at ~300 seconds
+  wait_for_staking_epoch 300 # Timeout at ~900 seconds
 
   echo "Starting test suite..."
   sleep 3
@@ -144,6 +144,30 @@ function wait_for_localnet_boot() {
 
   sleep 15  # Give some slack to ensure localnet is booted...
   echo "Localnet booted."
+}
+
+function wait_for_epoch() {
+  wait_for_localnet_boot "$2"
+  cur_epoch=0
+  echo "Waiting for epoch $1..."
+  until ((cur_epoch >= "$1")); do
+    cur_epoch=$(curl --silent --location --request POST "localhost:9500" \
+    --header "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"hmyv2_latestHeader","params":[],"id":1}' | jq .result.epoch)
+    if ((i > timeout)); then
+      echo "TIMEOUT REACHED"
+      exit 1
+    fi
+    sleep 3
+    i=$((i + 1))
+  done
+}
+
+function wait_for_staking_epoch() {
+  staking_epoch=$(curl --silent --location --request POST "localhost:9500" \
+    --header "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"hmyv2_getNodeMetadata","params":[],"id":1}' | jq '.result["chain-config"]["staking-epoch"]')
+  wait_for_epoch "$staking_epoch" "$1"
 }
 
 trap stop SIGINT SIGTERM EXIT
