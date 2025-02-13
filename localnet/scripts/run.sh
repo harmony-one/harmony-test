02c823145268aacd855f354873f4d61722f2ea00
Uladzislau Muraveika <uladzislau.muraveika@protofire.io>
2025-02-13 09:35:07 +0300

    fix(rosetta) - fix output redirection to have a way for manual local debug (#37)

--- localnet/scripts/run.sh
+++ localnet/scripts/run.sh
@@ -99,13 +99,17 @@ function rosetta_tests() {
   pwd
   echo "dir: $DIR/../configs/localnet_rosetta_test_s0.json"
   echo "[ROSETTA] check:construction s0"
-  rosetta-cli check:construction --configuration-file "$DIR/../configs/localnet_rosetta_test_s0.json" > /dev/null 2>1 || error=1
+  rosetta-cli check:construction --configuration-file \
+    "$DIR/../configs/localnet_rosetta_test_s0.json" > output_rossetta.log 2>&1 || error=1
   echo "[ROSETTA] check:data s0"
-  rosetta-cli check:data --configuration-file "$DIR/../configs/localnet_rosetta_test_s0.json" > /dev/null 2>1 || error=1
+  rosetta-cli check:data --configuration-file \
+    "$DIR/../configs/localnet_rosetta_test_s0.json" >> output_rossetta.log 2>&1 || error=1
   echo "[ROSETTA] check:construction s1"
-  rosetta-cli check:construction --configuration-file "$DIR/../configs/localnet_rosetta_test_s1.json" > /dev/null 2>1 || error=1
+  rosetta-cli check:construction --configuration-file \
+    "$DIR/../configs/localnet_rosetta_test_s1.json" >> output_rossetta.log 2>&1 || error=1
   echo "[ROSETTA] check:data s1"
-  rosetta-cli check:data --configuration-file "$DIR/../configs/localnet_rosetta_test_s1.json" > /dev/null 2>1 || error=1
+  rosetta-cli check:data --configuration-file \
+    "$DIR/../configs/localnet_rosetta_test_s1.json" >> output_rossetta.log 2>&1 || error=1
   echo -e "\n=== \e[38;5;0;48;5;255mFINISHED ROSETTA API TESTS\e[0m ===\n"
   if ((error == 1)); then
     echo "FAILED ROSETTA TESTS"
